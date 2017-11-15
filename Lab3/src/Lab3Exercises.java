039ac20e274b5140f83a1b0d6fc5826043f8b1b2
4010c8 <4010c8@guayaba.ece.uprm.edu>
2017-11-15 16:39:33 -0400

    Not much, just the name and the size of frames and display.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Anthony V. Rivera Torres");
+		JFrame myFrame = new JFrame("Anthony V.");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(860, 400);
 		myFrame.setSize(200, 200);
